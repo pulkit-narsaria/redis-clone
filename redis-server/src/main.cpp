cc71956addd9cb28daffd0fa986f9276e89f2adf
Pulkit Narsaria <pulkit.narsaria@philips.com>
2025-08-01 23:32:22 +0530

    [Implementation] Request Response Protocol (#3)
    * implementation of Request response protocol
    
    * addressed review comments

--- redis-server/src/main.cpp
+++ redis-server/src/main.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +7,7 @@
 #include <winsock2.h>
 
 SOCKET serverSocket;
+const size_t maxMessageSize = 4096;
 
 static void stop(const char* msg) 
 {
@@ -43,13 +45,104 @@ static void initializeServerSocket()
 	socketAddress.sin_port = htons(1234);
 	socketAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	int bindStatus = bind(serverSocket, (const struct sockaddr*)&socketAddress, sizeof(socketAddress));
+	int bindStatus = bind(serverSocket, (const sockaddr*)&socketAddress, sizeof(socketAddress));
 	if (bindStatus == SOCKET_ERROR) 
 		stop("Binding to the socket failed");
 
 	int listenStatus = listen(serverSocket, SOMAXCONN);
 	if (listenStatus == SOCKET_ERROR) 
 		stop("Listening to the client failed");
+}
+
+static int32_t readMessage(SOCKET fd, char* buf, int n)
+{
+	while (n > 0) 
+	{
+		int readSize = recv(fd, buf, n,0);
+		if (readSize <= 0) 
+			return -1;
+		
+		assert(readSize <= n);
+		n -= readSize;
+		buf += readSize;
+	}
+	return 0;
+}
+
+static int32_t writeMessage(SOCKET fd, const char* buf, int n)
+{
+	while (n > 0) 
+	{
+		int writeSize = send(fd, buf, n,0);
+		if (writeSize <= 0)
+			return -1;
+		
+		assert(writeSize <= n);
+		n -= writeSize;
+		buf += writeSize;
+	}
+	return 0;
+}
+
+static int32_t getRequest(SOCKET connfd)
+{
+	char rbuf[4 + maxMessageSize];
+	errno = 0;
+	int32_t err = readMessage(connfd, rbuf, 4);
+	if (err) {
+		dontStop(errno == 0 ? "EOF" : "read() error");
+		return err;
+	}
+
+	uint32_t len = 0;
+	memcpy(&len, rbuf, 4);
+	if (len > maxMessageSize) 
+	{
+		dontStop("too long");
+		return -1;
+	}
+
+	err = readMessage(connfd, &rbuf[4], len);
+	if (err) 
+	{
+		dontStop("read() error");
+		return err;
+	}
+
+	fprintf(stderr, "client says: %.*s\n", len, &rbuf[4]);
+
+	return 0;
+}
+
+static int32_t sendResponse(SOCKET connfd)
+{
+	const char reply[] = "world";
+	char wbuf[4 + sizeof(reply)];
+	uint32_t len = (uint32_t)strlen(reply);
+	memcpy(wbuf, &len, 4);
+	memcpy(&wbuf[4], reply, len);
+	uint32_t err = writeMessage(connfd, wbuf, 4 + len);
+	if (err)
+	{
+		dontStop("write() error");
+		return err;
+	}
+	return 0;
+}
+
+static int32_t processRequest(SOCKET connfd) 
+{
+	// according to request response protocol server first gets the request from client then
+	// responds to client with a response.
+
+	int32_t err = getRequest(connfd);
+	if (err)
+	{
+		dontStop("getRequest() error");
+		return err;
+	}
+
+	return sendResponse(connfd);
 }
 
 static void processServerClientActions(SOCKET clientSocket) {
@@ -65,7 +158,7 @@ static void processServerClientActions(SOCKET clientSocket) {
 	fprintf(stderr, "Client says: %s\n", receivedMessage);
 
 	char message[] = "world";
-	int sendStatus = send(clientSocket, message, strlen(message), 0);
+	int sendStatus = send(clientSocket, message, (int)strlen(message), 0);
 	if (sendStatus == SOCKET_ERROR)
 		dontStop("Sending to the client failed");
 }
@@ -83,12 +176,19 @@ static void connectToClient()
 			continue;
 		}
 
-		processServerClientActions(clientSocket);
+		while (true) 
+		{
+			int32_t err = processRequest(clientSocket);
+			if (err) 
+				break;
+		}
+
 		closesocket(clientSocket);
 	}
 }
 
-int main() {
+int main() 
+{
     initializeWinsock();
 	initializeServerSocket();
 
@@ -96,5 +196,6 @@ int main() {
 
     closesocket(serverSocket);
     WSACleanup();
-    return 0;
+
+	return 0;
 }
