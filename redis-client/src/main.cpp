d078cec6b94b622d1fbffdbe90016ae68baa6dac
Pulkit Narsaria <Pulkit.Narsaria@philips.com>
2025-08-07 23:16:24 +0530

    implementation of event loop to handle multiple clients simultaneously

--- redis-client/src/main.cpp
+++ redis-client/src/main.cpp
@@ -4,14 +4,23 @@
 #include <stdio.h>
 #include <errno.h>
 #include <winsock2.h>
+#include <vector>
+#include <string>
+#include <assert.h>
 
 SOCKET clientSocket;
+const size_t maxMessageSize = 32 << 20;
 
 static void stop(const char* msg)
 {
 	int err = WSAGetLastError();
 	fprintf(stderr, "[%d] %s\n", err, msg);
 	exit(EXIT_FAILURE);
+}
+
+static void dontStop(const char* message)
+{
+	fprintf(stderr, "%s\n", message);
 }
 
 static void initializeWinsock() 
@@ -38,32 +47,115 @@ static void initializeClientSocket()
 		stop("Connection to the server failed");
 }
 
-static void sendMessage(const char* msg)
+static int32_t readMessage(SOCKET clientSocket, char* message, size_t messageLength)
 {
-	int messageLength = (int)strlen(msg);
-	if (send(clientSocket, msg, messageLength, 0) == SOCKET_ERROR)
-		stop("Sending message to server failed");
+	while (messageLength > 0) 
+	{
+		size_t readSize = recv(clientSocket, message, messageLength, 0);
+		if (readSize <= 0) return -1;
+		assert((size_t)readSize <= messageLength);
+		messageLength -= (size_t)readSize;
+		message += readSize;
+	}
+	return 0;
 }
 
-static void receiveMessage()
+static int32_t writeMessage(SOCKET fd, const char* buf, int n)
 {
-	char message[64] = {};
-	int n = recv(clientSocket, message, sizeof(message) - 1, 0);
+	while (n > 0) 
+	{
+		int writeSize = send(fd, buf, n,0);
+		if (writeSize <= 0) 
+			return -1;
+		assert(writeSize <= n);
+		n -= writeSize;
+		buf += writeSize;
+	}
+	return 0;
+}
 
-	if (n == SOCKET_ERROR) 
-		stop("Receiving message from server failed");
-	
-	printf("Server says: %s\n", message);
+static int32_t sendRequest(SOCKET clientSocket, const char* message, size_t messageSize)
+{
+	if (messageSize > maxMessageSize) {
+		return -1;
+	}
+
+	std::vector<char> sendMessage;
+	sendMessage.insert(sendMessage.end(), (char*)&messageSize, (char*)&messageSize + 4);
+	sendMessage.insert(sendMessage.end(), message, message + messageSize);
+
+	return writeMessage(clientSocket, sendMessage.data(), sendMessage.size());
+}
+
+static int32_t getResponse(SOCKET clientSocket)
+{
+	std::vector<char> getMessage;
+	getMessage.resize(4);
+	errno = 0;
+	int32_t err = readMessage(clientSocket, &getMessage[0], 4);
+	if (err) 
+	{
+		dontStop(errno == 0 ? "EOF" : "read() error");
+		return err;
+	}
+
+	uint32_t len = 0;
+	memcpy(&len, getMessage.data(), 4);
+	if (len > maxMessageSize) 
+	{
+		dontStop("too long");
+		return -1;
+	}
+
+	getMessage.resize(4 + len);
+	err = readMessage(clientSocket, &getMessage[4], len);
+	if (err) {
+		dontStop("read() error");
+		return err;
+	}
+
+	// do something
+	printf("len:%u data:%.*s\n", len, len < 100 ? len : 100, &getMessage[4]);
+	return 0;
+}
+
+static int32_t query()
+{
+	// according to request response protocol first client sends request to the server 
+	// then server processes the request and sends response back to the client
+
+	std::vector<std::string> query_list = { "hello1", "hello2", "hello3", std::string(maxMessageSize, 'z'), "hello5", };
+
+	for (const std::string& s : query_list) 
+	{
+		int32_t err = sendRequest(clientSocket, (char*)s.data(), s.size());
+		if (err) 
+		{
+			dontStop("sendRequest() error");
+			return err;
+		}
+	}
+
+	for (size_t i = 0; i < query_list.size(); ++i)
+	{
+		int32_t err = getResponse(clientSocket);
+		if (err)
+		{
+			dontStop("getResponse() error");
+			return err;
+		}
+	}
+
+	return 0;
 }
 
 int main() 
 {
     initializeWinsock();
 	initializeClientSocket();
-
-	sendMessage("hello");
-	receiveMessage();
     
+	query();
+	    
     closesocket(clientSocket);
     WSACleanup();
     return 0;
--- redis-server/src/main.cpp
+++ redis-server/src/main.cpp
@@ -4,19 +4,45 @@
 #include <stdio.h>
 #include <errno.h>
 #include <winsock2.h>
+#include <vector>
+#include <assert.h>
 
 SOCKET serverSocket;
+const size_t maxMessageSize = 32 << 20;
 
-static void stop(const char* msg) 
+struct ClientConnection 
+{
+	SOCKET socket = -1;
+
+	// application's intention, for the event loop
+	bool want_read = false;
+	bool want_write = false;
+	bool want_close = false;
+
+	std::vector<char> clientMessage;
+	std::vector<char> serverResponseMessage;
+};
+
+static void stop(const char* message) 
 {
 	int err = WSAGetLastError();
-	fprintf(stderr, "[%d] %s\n", err, msg);
+	fprintf(stderr, "[%d] %s\n", err, message);
 	exit(EXIT_FAILURE);
 }
 
-static void dontStop(const char* msg)
+static void dontStop(const char* message)
 {
-	fprintf(stderr, "%s\n", msg);
+	fprintf(stderr, "%s\n", message);
+}
+
+static void addToMessage(std::vector<char>& messageBuffer, const char* message, size_t messageSize)
+{
+	messageBuffer.insert(messageBuffer.end(), message, message + messageSize);
+}
+
+static void removeFromMessage(std::vector<char>& messageBuffer, size_t messageSize)
+{
+	messageBuffer.erase(messageBuffer.begin(), messageBuffer.begin() + messageSize);
 }
 
 static void initializeWinsock() 
@@ -25,6 +51,13 @@ static void initializeWinsock()
     int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (result != 0) 
         stop("WSAStartup failed");
+}
+
+static void setSocketToNonBlockingIo(SOCKET &socket)
+{
+	u_long mode = 1;
+	if (ioctlsocket(socket, FIONBIO, &mode) != 0)
+		stop("ioctlsocket error");
 }
 
 static void initializeServerSocket()
@@ -47,44 +80,207 @@ static void initializeServerSocket()
 	if (bindStatus == SOCKET_ERROR) 
 		stop("Binding to the socket failed");
 
+	setSocketToNonBlockingIo(serverSocket);
+
 	int listenStatus = listen(serverSocket, SOMAXCONN);
 	if (listenStatus == SOCKET_ERROR) 
 		stop("Listening to the client failed");
 }
 
-static void processServerClientActions(SOCKET clientSocket) {
-	char receivedMessage[64] = {};
-	int receiveStatus = recv(clientSocket, receivedMessage, sizeof(receivedMessage) - 1, 0);
-
-	if (receiveStatus == SOCKET_ERROR) 
+static void writeMessage(ClientConnection* clientConnection)
+{
+	assert(clientConnection->serverResponseMessage.size() > 0);
+	size_t messageLength = send(clientConnection->socket, &clientConnection->serverResponseMessage[0], (int)clientConnection->serverResponseMessage.size(), 0);
+	
+	if (messageLength < 0 && errno == EAGAIN) return;
+	
+	if (messageLength < 0) 
 	{
-		dontStop("Receiving from the client failed");
+		dontStop("write() error");
+		clientConnection->want_close = true;
 		return;
 	}
 
-	fprintf(stderr, "Client says: %s\n", receivedMessage);
+	removeFromMessage(clientConnection->serverResponseMessage, (size_t)messageLength);
 
-	char message[] = "world";
-	int sendStatus = send(clientSocket, message, strlen(message), 0);
-	if (sendStatus == SOCKET_ERROR)
-		dontStop("Sending to the client failed");
+	if (clientConnection->serverResponseMessage.size() == 0) 
+	{
+		clientConnection->want_read = true;
+		clientConnection->want_write = false;
+	}
 }
 
-static void connectToClient()
+static bool processOneReadRequest(ClientConnection* clientConnection) 
 {
-	while (true) 
+	if (clientConnection->clientMessage.size() < 4) return false;
+	
+	uint32_t clientMessageLength = 0;
+	memcpy(&clientMessageLength, clientConnection->clientMessage.data(), 4);
+
+	if (clientMessageLength > maxMessageSize) 
 	{
-		struct sockaddr_in clientAddress = {};
-		int addressLength = sizeof(clientAddress);
-		SOCKET clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &addressLength);
-		if (clientSocket == INVALID_SOCKET) 
+		dontStop("too long");
+		clientConnection->want_close = true;
+		return false;
+	}
+
+	if (4 + clientMessageLength > clientConnection->clientMessage.size()) return false;
+	
+	const char* clientMessageText = &clientConnection->clientMessage[4];
+
+	printf("client says: len:%d data:%.*s\n", clientMessageLength, clientMessageLength < 100 ? clientMessageLength : 100, clientMessageText);
+
+	addToMessage(clientConnection->serverResponseMessage, (const char*)&clientMessageLength, 4);
+	addToMessage(clientConnection->serverResponseMessage, clientMessageText, clientMessageLength);
+
+	removeFromMessage(clientConnection->clientMessage, 4 + clientMessageLength);
+	
+	return true;
+}
+
+static void readMessage(ClientConnection* clientConnection)
+{
+	char clientMessage[64 * 1024];
+	size_t rv = recv(clientConnection->socket, clientMessage, sizeof(clientMessage), 0);
+
+	if (rv < 0 && errno == EAGAIN) return;
+	
+	if (rv < 0) 
+	{
+		dontStop("read() error");
+		clientConnection->want_close = true;
+		return;
+	}
+
+	if (rv == 0) 
+	{
+		clientConnection->clientMessage.size() == 0 ? dontStop("client closed") : stop("unexpected EOF");
+		clientConnection->want_close = true;
+		return;
+	}
+
+	addToMessage(clientConnection->clientMessage, clientMessage, (size_t)rv);
+
+	while (processOneReadRequest(clientConnection)) {}
+	
+	if (clientConnection->serverResponseMessage.size() > 0) 
+	{
+		clientConnection->want_read = false;
+		clientConnection->want_write = true;
+		return writeMessage(clientConnection);
+	}
+}
+
+static ClientConnection* acceptClientConnection(SOCKET socket) 
+{
+	struct sockaddr_in clientSocketAddress = {};
+	int addrlen = sizeof(clientSocketAddress);
+	SOCKET connfd = accept(socket, (struct sockaddr*)&clientSocketAddress, &addrlen);
+	
+	if (connfd < 0) 
+	{
+		dontStop("accept() error");
+		return NULL;
+	}
+
+	uint32_t ip = clientSocketAddress.sin_addr.s_addr;
+	
+	fprintf(stderr, "new client from %u.%u.%u.%u:%u\n", ip & 255, (ip >> 8) & 255, (ip >> 16) & 255, ip >> 24, ntohs(clientSocketAddress.sin_port));
+
+	setSocketToNonBlockingIo(connfd);
+
+	ClientConnection* clientConnection = new ClientConnection();
+	clientConnection->socket = connfd;
+	clientConnection->want_read = true;
+	return clientConnection;
+}
+
+static void handleClientConnections(std::vector<struct pollfd>& poll_args, std::vector<ClientConnection*>& clientConnections)
+{
+	if (poll_args[0].revents)
+	{
+		if (ClientConnection* clientConnection = acceptClientConnection(serverSocket))
 		{
-			dontStop("Could not connect to the client");
+			if (clientConnections.size() <= (size_t)clientConnection->socket)
+				clientConnections.resize(clientConnection->socket + 1);
+
+			assert(!clientConnections[clientConnection->socket]);
+			clientConnections[clientConnection->socket] = clientConnection;
+		}
+	}
+}
+
+static bool setPollArgs(std::vector<struct pollfd> &poll_args,std::vector<ClientConnection*> &clientConnections)
+{
+	poll_args.clear();
+
+	struct pollfd serverPollFd = { serverSocket, POLLIN, 0 };
+	poll_args.push_back(serverPollFd);
+
+	for (const auto& client : clientConnections)
+	{
+		if (!client) continue;
+
+		struct pollfd clientPollFd = { client->socket, 0, 0 };
+
+		if (client->want_read) clientPollFd.events |= POLLIN;
+		if (client->want_write) clientPollFd.events |= POLLOUT;
+
+		poll_args.push_back(clientPollFd);
+	}
+
+	// wait for readiness
+	int rv = WSAPoll(poll_args.data(), (u_long)poll_args.size(), -1);
+
+	if (rv < 0 && errno == EINTR) return false;
+	if (rv < 0) stop("poll");
+
+	return true;
+}
+
+static void handleClientEvents(std::vector<struct pollfd>& poll_args, std::vector<ClientConnection*>& clientConnections)
+{
+	for (size_t i = 1; i < poll_args.size(); ++i)
+	{
+		uint32_t ready = poll_args[i].revents;
+
+		if (ready == 0)
+		{
 			continue;
 		}
 
-		processServerClientActions(clientSocket);
-		closesocket(clientSocket);
+		ClientConnection* clientConnection = clientConnections[poll_args[i].fd];
+
+		if (ready & POLLIN)
+		{
+			assert(clientConnection->want_read);
+			readMessage(clientConnection);
+		}
+
+		if (ready & POLLOUT)
+		{
+			assert(clientConnection->want_write);
+			writeMessage(clientConnection);
+		}
+
+		if ((ready & POLLERR) || clientConnection->want_close) {
+			(void)closesocket(clientConnection->socket);
+			clientConnections[clientConnection->socket] = NULL;
+			delete clientConnection;
+		}
+	}
+}
+
+static void handleClients()
+{
+	std::vector<ClientConnection*> clientConnections;
+	std::vector<struct pollfd> poll_args;
+
+	while (true) 
+	{
+		setPollArgs(poll_args, clientConnections);
+		handleClientConnections(poll_args, clientConnections);
+		handleClientEvents(poll_args, clientConnections);
 	}
 }
 
@@ -92,7 +288,7 @@ int main() {
     initializeWinsock();
 	initializeServerSocket();
 
-	connectToClient();
+	handleClients();
 
     closesocket(serverSocket);
     WSACleanup();
